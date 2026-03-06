f653f56525a8545a91fdc314552d899da33f0db1
alsydybrkt-glitch <alsydybrkt@gmail.com>
2026-03-06 15:40:45 +0200

    add vercel config

--- src/component/HeroSlider.jsx
+++ src/component/HeroSlider.jsx
@@ -1,95 +1,89 @@
 import { Link, useNavigate } from "react-router-dom";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
+// Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-
-// import required modules
 import { Autoplay, Pagination } from "swiper/modules";
 
 function HeroSlider() {
   const navigate = useNavigate();
+
   const handleShopNowClick = () => {
     navigate("/shop");
   };
-
-  // force redeploy
-
 
   return (
     <div className="heroSlider">
       <div className="container">
         <Swiper
           autoplay={{ delay: 2000, disableOnInteraction: false }}
-          pagination={true}
+          pagination={{ clickable: true }}
           modules={[Autoplay, Pagination]}
           className="mySwiper"
         >
+          {/* Slide 1 */}
           <SwiperSlide>
             <div className="content">
               <h4>introducing the new</h4>
+
               <h3>
-                Microsoft XBox <br /> 360 Controller
+                Microsoft XBox <br />
+                360 Controller
               </h3>
+
               <p>Windows Xb 10/7/8/ ps3 / TV Box</p>
+
               <Link to="/shop" onClick={handleShopNowClick} className="btn">
                 Shop Now
               </Link>
             </div>
 
-            <img
-              src="/src/React Ecommerce Reda Tech/img/banner_Hero1.jpg"
-              alt="hero1"
-            />
-
-            <img src="/images/img/banner_Hero1.jpg" alt="hero1" />
-
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="content">
-              <h4>introducing the new</h4>
-              <h3>
-                Microsoft XBox <br /> 360 Controller
-              </h3>
-              <p>Windows Xb 10/7/8/ ps3 / TV Box</p>
-              <Link onClick={handleShopNowClick} to="/" className="btn">
-                Shop Now
-              </Link>
-            </div>
-
-            <img
-              src="/src/React Ecommerce Reda Tech/img/banner_Hero2.jpg"
-              alt="hero1"
-            />
-
             <img src="/images/img/banner_Hero2.jpg" alt="hero2" />
-
           </SwiperSlide>
+
+          {/* Slide 2 */}
           <SwiperSlide>
             <div className="content">
               <h4>introducing the new</h4>
+
               <h3>
-                Microsoft XBox <br /> 360 Controller
+                Microsoft XBox <br />
+                360 Controller
               </h3>
+
               <p>Windows Xb 10/7/8/ ps3 / TV Box</p>
-              <Link onClick={handleShopNowClick} to="/" className="btn">
+
+              <Link to="/shop" onClick={handleShopNowClick} className="btn">
                 Shop Now
               </Link>
             </div>
+            <img src="/images/img/banner_Hero1.jpg" alt="hero1" />
+          </SwiperSlide>
 
-            <img
-              src="/src/React Ecommerce Reda Tech/img/banner_Hero3.jpg"
-              alt="hero1"
-            />
+          {/* Slide 3 */}
+          <SwiperSlide>
+            <div className="content">
+              <h4>introducing the new</h4>
+
+              <h3>
+                Microsoft XBox <br />
+                360 Controller
+              </h3>
+
+              <p>Windows Xb 10/7/8/ ps3 / TV Box</p>
+
+              <Link to="/shop" onClick={handleShopNowClick} className="btn">
+                Shop Now
+              </Link>
+            </div>
 
             <img src="/images/img/banner_Hero3.jpg" alt="hero3" />
-
           </SwiperSlide>
         </Swiper>
       </div>
     </div>
   );
 }
+
 export default HeroSlider;
